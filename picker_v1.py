26ac50f9b7b4d9278d8a814dca3c954de6266224
Flinchum, Brady (L&W, Waite Campus) <Brady.Flinchum@csiro.au>
2019-09-30 21:38:23 +0930

    no changes but I need to do another pull and it won't let me unless I save this??

--- picker_v1.py
+++ picker_v1.py
@@ -137,6 +137,7 @@ def bpData(data,lf,hf,nq,order):
 class pickingModule:
     def __init__(self, x, t, data, shotLoc, pickFileName, gx):
         self.dt = np.round(np.diff(t)[0],decimals=4)
+        
         #DEFINE VARIABLES
         if os.path.exists(pickFileName):
             tempData = np.loadtxt(pickFileName)
@@ -158,6 +159,7 @@ class pickingModule:
         #SET UP FIGURE SPACES
         self.fig1 = plt.figure(1,dpi=100,figsize=[8,7])
         self.fig2 = plt.figure(2,dpi=100,figsize=[4,7])
+
         gs = gridspec.GridSpec(5,1, height_ratios=[5,0.5,0.25,0.25,0.25])
 
         self.ax1 = self.fig1.add_subplot(gs[0]) #Main data axes
@@ -282,7 +284,6 @@ class pickingModule:
         #Activate the sliders    
         self.mainAmpSlider.on_changed(updateAmp_main)
         self.mainTimeSlider.on_changed(updateAmp_main)
-
         #OPEN/CREATE EVENT HANDLERS FOR PICKIGN AND DELETING
         #define funciton to move the traces
         def switchTraces(event):            
@@ -329,8 +330,8 @@ class pickingModule:
                 self.ax4.set_ylabel('Time (s)')
                 self.ax4.set_title(str(x[self.traceNum]) + ' m')
                 self.fig2.canvas.draw()
-        
 
+        
             
         def whenReleased(event):
             MAX_CLICK_LENGTH = 0.2
@@ -405,9 +406,8 @@ class pickingModule:
         self.fig2.canvas.mpl_connect('key_press_event',switchTraces)
         self.fig2.canvas.mpl_connect('button_press_event',whenClicked)
         self.fig2.canvas.mpl_connect('button_release_event',whenReleased)
-
+        
 if __name__ == '__main__':
-
     
     applyBPFilt = True
     #applyBPFilt = False
@@ -437,7 +437,7 @@ if __name__ == '__main__':
     data = normalizeTraces(data)
         
     a = pickingModule(x,t,data,shotLoc,pickFile,gx)
-    
+
     
 #***************THESE ARE PLOTS THAT I WANT to Incorporate into the GUI*******
 #    #They are built from the pick data read from a file during the GUI
